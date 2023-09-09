ebd71c40ba299df479bd8118c0e40af119cfece4
João Victor <joaovba2002@alu.ufc.br>
2023-09-09 11:44:22 -0300

    Navegação e layout básico de sidebar

---
+++ main.js
@@ -0,0 +1,8 @@
+/**
+ * Esta função é utilizada para ocultar e visualizar 
+ * o sidebar quando o tamanho da tela estiver pequeno
+ */
+function toggleSidebar() {
+  var sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("active");
+}
