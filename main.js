e04b75725dc149fc0b02da03653474c4aefa7004
VladiaHFerr <vladiahelenff@gmail.com>
2023-09-15 08:27:14 -0300

    Tela de photocards e artistas

--- main.js
+++ main.js
@@ -94,3 +94,22 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
